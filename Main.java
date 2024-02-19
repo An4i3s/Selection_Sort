58d857edc998152b29b8319618e51c23ecdca516
Anaïs <76781733+An4i3s@users.noreply.github.com>
2024-02-19 11:47:36 +0100

    Inserito commenti a algoritmo

--- Main.java
+++ Main.java
@@ -13,14 +13,19 @@ public class Main {
 
 
                 for (int i = 0; i<ints.length-1; i++){
+                    ////Partiamo dall' i-esimo elemento
                     int minValue = i;
 
                     for (int j = i+1; j<ints.length; j++){
+                        //Ogni volta che nell’iterazione troviamo un elemento piú
+                        //piccolo di minimo facciamo puntare minimo all’elemento trovato
                         if (ints[minValue] > ints[j]){
                             minValue = j;
                         }
                     }
 
+                    //Se minimo e diverso dall' elemento di partenza
+                    //allora avviene lo scambio
                     if (minValue != i){
                         int k = ints[minValue];
                         ints[minValue] = ints[i];
